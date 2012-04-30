39b217aabc5bd86d8eb75682f37d1d63a8775e89
Keith Alexander <github@keithalexander.co.uk>
2012-04-30 12:10:15 +0100

    added sparql endpoint, meta?about, and meta/changesets

--- index.php
+++ index.php
@@ -2,10 +2,20 @@
 require 'fatfree/lib/base.php';
 require 'rdfstore.php';
 F3::route('GET /@store/services/sparql',
-	function() {
-		
-	}
+  function() {
+    /* initiates ARC's built in sparql endpoint */
+    getStore(F3::get('PARAMS["store"]'))->go(); 
+  }
 );
+
+F3::route('GET /@store/meta', function(){
+  if(isset($_GET['about'])){
+    $_SERVER['QUERY_STRING'] = 'query='.urlencode("DESCRIBE <{$_GET['about']}>");
+    getStore(F3::get('PARAMS["store"]'))->go(); 
+  }
+});
+
+F3::route('POST /@store/meta/changesets', 'process_changeset_request');
 
 F3::run();
 
--- rdfstore.php
+++ rdfstore.php
@@ -2,9 +2,15 @@
 
 /* ARC2 static class inclusion */ 
 include_once('arc/ARC2.php');
+define('rdf_ns',  'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+define('cs_ns', 'http://purl.org/vocab/changeset/schema#');
 
-function getStore($storename){
+function getStore($storename, $writable=false){
   /* configuration */ 
+  $endpoint_features = array('select', 'construct', 'ask', 'describe');
+  if($writable){
+    $endpoint_features = array_merge($endpoint_features, array('load', 'insert', 'delete'));
+  }
   $config = array(
     /* db */
       'db_host' => DB_HOST, /* optional, default is localhost */
@@ -13,13 +19,64 @@ function getStore($storename){
       'db_pwd' =>  DB_PWD,
       /* store name (= table prefix) */
       'store_name' => $storename,
+    /* endpoint */
+    'endpoint_features' =>  $endpoint_features ),
+    'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+    'endpoint_read_key' => '', /* optional */
+//    'endpoint_write_key' => 'somekey', /* optional */
+    'endpoint_max_limit' => 250, /* optional */
+      
     );
     /* instantiation */
-  $store = ARC2::getStore($config);
-  if (!$store->isSetUp()) {
-    $store->setUp();
-  }
-    return $store;
+  $store = ARC2::getStoreEndpoint($config);
+
+  return $store;
 }
+
+function dereifyStatementToNtriples($statement_properties){
+  $rdf_index = array();
+  $subject = $statement_properties[rdf_ns.'subject'][0];['value']
+  $predicate = $statement_properties[rdf_ns.'predicate'][0]['value'];
+  $object = $statement_properties[rdf_ns.'object'][0];
+  $rdf_index[$subject][$predicate] = array($object);
+  $serialiser = ARC2::getNTriplesSerializer();
+  return $serialiser->getSerializedIndex($rdf_index);
+}
+
+function process_changeset_request(){
+  $store = getStore(F3::get('PARAMS["store"]'),'writable');
+  $rdf_index = $store->toIndex(file_get_contents('php://input'),1);
+
+  $triplesToInsert = '';
+  $triplesToDelete = '';
+
+  foreach($rdf_index as $s => $props){
+    if(isset($props[cs_ns.'addition'])){
+      foreach($props[cs_ns.'addition'] as $addition){
+          $statement = $rdf_index[$addition['value']];
+          $triplesToInsert.= dereifyStatementToNtriples($statement);
+        }
+    }
+    if(isset($props[cs_ns.'removal'])){
+      foreach($props[cs_ns.'removal'] as $removal){
+          $statement = $rdf_index[$removal['value']];
+          $triplesToDelete.= dereifyStatementToNtriples($statement);
+      }
+    }
+  }
+
+  $store->replace($triplesToDelete, 'urn:defaultGraph', $triplesToInsert);
+  
+  if(empty($store->getErrors())){
+    header("HTTP/1.1 204 No Content");
+    exit;
+  } else {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo implode("\n", $store->getErrors());
+    exit;
+  }
+}
+
+
 
 ?>
